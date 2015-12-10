b5bcd6026d35432ff38155f18cc0fde5fcc30ff0
gisellegk <gisellegk@gmail.com>
2015-12-10 13:47:02 -0500

    move stuff to settings at top

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 google.load('visualization', 1.0);
 
+var eventName = "Drama Club Sign In"
+var database = "https://docs.google.com/a/wethersfield.me/spreadsheets/d/1NcGy2e_nzsDuX-BkHcw0UlzSTdIkbnn0ONIN6stE9Ag/edit";
+var formInfo = {
+  "link": "https://docs.google.com/a/wethersfield.me/forms/d/1jKSxKrEwKGYr5XmJ6RFmn6WgZNCHxtoqHSP2FPMGBVw/formResponse";
+  "timeIn": "entry.1826631571",
+  "sid": "entry.846193839", 
+  "firstName": "entry.1810641923", 
+  "lastName":"entry.2112623826", 
+  "grade": "entry.421600000",
+  "team": "entry.1105692196"
+}
 function getStudent(){
 
   var id = $("#sid").val();
@@ -10,7 +21,7 @@ function getStudent(){
   }
 
   var opts = {sendMethod: 'auto'};
-  var query = new google.visualization.Query('https://docs.google.com/a/wethersfield.me/spreadsheets/d/1NcGy2e_nzsDuX-BkHcw0UlzSTdIkbnn0ONIN6stE9Ag/edit', opts);
+  var query = new google.visualization.Query(database, opts);
   query.setQuery('select * where A =' + id);
   query.send(handleQueryResponse);
   console.log('sent');
@@ -54,19 +65,19 @@ function handleQueryResponse(response){
 function postToGoogle(student) {
   var form = document.createElement("form");
 
-  form.action = "https://docs.google.com/a/wethersfield.me/forms/d/1jKSxKrEwKGYr5XmJ6RFmn6WgZNCHxtoqHSP2FPMGBVw/formResponse";
+  form.action = formInfo.link;
   form.method = "POST";
   form.id="ss-form";
   form.target = "my_iframe";
   var timeIn = new Date();
 
   form.innerHTML = [
-    "<input id='entry_1826631571' name = 'entry.1826631571' value = '" + timeIn + "'/>",
-    "<input id='entry_846193839' name = 'entry.846193839' value = '" + student.sid + "'/>",
-    "<input id='entry_1810641923' name = 'entry.1810641923' value = '" + student.firstName + "'/>",
-    "<input id='entry_2112623826' name = 'entry.2112623826' value = '" + student.lastName + "'/>",
-    "<input id='entry_421600000' name = 'entry.421600000' value = '" + student.grade + "'/>",
-    "<input id='entry_1105692196' name = 'entry.1105692196' value = '" + student.team + "'/>"
+    "<input id='entry_1826631571' name = '" + formInfo.timeIn + "'' value = '" + timeIn + "'/>",
+    "<input id='entry_846193839' name = '" + formInfo.sid + "' value = '" + student.sid + "'/>",
+    "<input id='entry_1810641923' name = '" + formInfo.firstName + "' value = '" + student.firstName + "'/>",
+    "<input id='entry_2112623826' name = '" + formInfo.lastName + "' value = '" + student.lastName + "'/>",
+    "<input id='entry_421600000' name = '" + formInfo.grade + "' value = '" + student.grade + "'/>",
+    "<input id='entry_1105692196' name = '" + formInfo.team + "' value = '" + student.team + "'/>"
   ].join("");
 
   form.submit();
