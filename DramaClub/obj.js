9a2337c1259966fe8d47418c10aa422a830ca97c
gisellegk <gisellegk@gmail.com>
2015-12-17 13:09:14 -0500

    ..

---
+++ DramaClub/obj.js
@@ -0,0 +1,11 @@
+var eventName = "Drama Club Sign In"
+var database = "https://docs.google.com/a/wethersfield.me/spreadsheets/d/1NcGy2e_nzsDuX-BkHcw0UlzSTdIkbnn0ONIN6stE9Ag/edit";
+var formInfo = {
+  "link": "https://docs.google.com/a/wethersfield.me/forms/d/1J232R3D0ycbSWN1P3m8Spc00P_ArerYr8zcdBEVNN1U/formResponse",
+  "timeIn": "entry.1826631571",
+  "sid": "entry.846193839", 
+  "firstName": "entry.1810641923", 
+  "lastName":"entry.2112623826", 
+  "grade": "entry.421600000",
+  "team": "entry.1105692196"
+}
---
+++ Office/obj.js
@@ -0,0 +1,11 @@
+var eventName = "Office Check-In"
+var database = "https://docs.google.com/a/wethersfield.me/spreadsheets/d/1NcGy2e_nzsDuX-BkHcw0UlzSTdIkbnn0ONIN6stE9Ag/edit";
+var formInfo = {
+  "link": "https://docs.google.com/a/wethersfield.me/forms/d/1Pfm71t9eOid-vuxl2_lm5zFDN_A_-2uy_AddhFU5Ja8/formResponse",
+  "timeIn": "entry.1826631571",
+  "sid": "entry.846193839", 
+  "firstName": "entry.1810641923", 
+  "lastName":"entry.2112623826", 
+  "grade": "entry.421600000",
+  "team": "entry.1105692196"
+}
