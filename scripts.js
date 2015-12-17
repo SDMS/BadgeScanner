3dec9c21433433d101c316d191ad05b50586bd28
gisellegk <gisellegk@gmail.com>
2015-12-17 07:42:42 -0500

    remove template stuff

--- scripts.js
+++ scripts.js
@@ -1,17 +1,6 @@
 google.load('visualization', 1.0);
 
 var timeout;
-var eventName = "Template (this doesn't work)"
-var database = "https://docs.google.com/a/wethersfield.me/spreadsheets/d/1NcGy2e_nzsDuX-BkHcw0UlzSTdIkbnn0ONIN6stE9Ag/edit";
-var formInfo = {
-  "link": "https://docs.google.com/a/wethersfield.me/forms/d/<docid>/formResponse",
-  "timeIn": "entry.1826631571",
-  "sid": "entry.846193839", 
-  "firstName": "entry.1810641923", 
-  "lastName":"entry.2112623826", 
-  "grade": "entry.421600000",
-  "team": "entry.1105692196"
-}
 
 $( document ).ready(function() {
   $("#title").html(eventName);
