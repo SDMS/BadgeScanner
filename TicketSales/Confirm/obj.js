028db2ca34e35d4c38d9e33d8b5b90cef5583206
gisellegk <gisellegk@gmail.com>
2016-01-12 11:43:42 -0500

    add dance ticket stuff

---
+++ TicketSales/Confirm/obj.js
@@ -0,0 +1,11 @@
+var eventName = "Dance Ticket Confirmation Page"
+var database = "https://docs.google.com/a/wethersfield.me/spreadsheets/d/1ijTPbVQm7pjvQNg1ptjVuS3ycdkDgCU4eBWXnnrj4Ns/edit";
+var formInfo = {
+  "link": "https://docs.google.com/a/wethersfield.me/forms/d/1hkIxNeu4CbSyodpXp56hH6Qopd7WSsuPmFISLhlyQHg/formResponse",
+  "timeIn": "entry.1826631571",
+  "sid": "entry.846193839", 
+  "firstName": "entry.1810641923", 
+  "lastName":"entry.2112623826", 
+  "grade": "entry.421600000",
+  "team": "entry.1105692196"
+}
